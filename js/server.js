650eeab9140618feed8e8ab4645a947ff2b6afc3
cilvin <cilvin33@users.noreply.github.com>
2019-06-17 16:04:16 -0700

    Add multiple modules to server.js

---
+++ js/server.js
@@ -0,0 +1,27 @@
+const http = require('http'),
+    url = require('url');
+
+http.createServer((request, response) => {
+    var addr = request.url,
+        q = url.parse(addr, true),
+        filePath = '';
+
+    if (q.pathname.includes('documenatation')) {
+        filePath = (__dirname + '/documentation.html');
+    } else {
+        filePath = 'index.html';
+    }
+
+    fs.readFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Added to log.');
+        }
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+    });
+    
+        response.end('Success!\n');
+}).listen(8080);
+
+console.log('My first Node test server is running on Port 8080.');
