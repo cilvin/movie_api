04dc321c71955279614d9ec76231d3991dd91eda
cilvin <cilvin33@users.noreply.github.com>
2019-07-18 17:01:10 -0700

    Integrate bcrypt into model.js
    These two pieces of code added hash the password before it enters the mongodb database and validate whether or not the same hashed password was entered.

--- model.js
+++ model.js
@@ -1,5 +1,6 @@
 /*global require:true   module:true */
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 var movieSchema = mongoose.Schema({
     Title : {type: String, required : true},
@@ -25,7 +26,15 @@ var userSchema = mongoose.Schema({
     Birthday: Date,
     FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 
-});
+}); 
+
+userSchema.statics.hashPassword = function(password) {
+    return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.Password); 
+};
 
 var Movie = mongoose.model('Movie', movieSchema);
 var User = mongoose.model('User', userSchema);
